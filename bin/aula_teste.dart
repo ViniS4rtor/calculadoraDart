e68c6bcbc47f56e3d568aacd26148e520e7c8a41
Vinícius Sartor <sartor.vini@gmail.com>
2025-07-16 16:15:46 -0300

    Initial commit

---
+++ bin/aula_teste.dart
@@ -0,0 +1,59 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+  while (true) {
+    print('Digite o número 1 (ou "sair" para encerrar):');
+    var entrada1 = stdin.readLineSync();
+    if (entrada1 == null || entrada1.toLowerCase() == 'sair') break;
+    var num1 = double.tryParse(entrada1);
+    if (num1 == null) {
+      print('Número inválido.');
+      continue;
+    }
+
+    List<String> operacoes = ['+', '-', '*', '/'];
+    print('Digite a operação $operacoes (ou "sair" para encerrar):');
+    var operacao = stdin.readLineSync();
+    if (operacao == null || operacao.toLowerCase() == 'sair') break;
+
+    print('Digite o número 2 (ou "sair" para encerrar):');
+    var entrada2 = stdin.readLineSync();
+    if (entrada2 == null || entrada2.toLowerCase() == 'sair') break;
+    var num2 = double.tryParse(entrada2);
+    if (num2 == null) {
+      print('Número inválido.');
+      continue;
+    }
+
+    if (operacao != '+' &&
+        operacao != '-' &&
+        operacao != '*' &&
+        operacao != '/') {
+      print('Operação inválida. Digitou duas operações ou mais.');
+      continue;
+    }
+
+    switch (operacao) {
+      case '+':
+        print('Resultado: ${num1 + num2}');
+        break;
+      case '-':
+        print('Resultado: ${num1 - num2}');
+        break;
+      case '*':
+        print('Resultado: ${num1 * num2}');
+        break;
+      case '/':
+        if (num2 == 0) {
+          print('Erro: Divisão por zero não é permitida.');
+        } else {
+          print('Resultado: ${num1 / num2}');
+        }
+        break;
+      default:
+        print('Operação inválida');
+    }
+    print('-----------------------------');
+  }
+  print('Calculadora encerrada.');
+}
